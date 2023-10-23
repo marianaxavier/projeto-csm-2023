d1c950507f42feb9a20c8f91edd5053aa78cf7cf
Mariana Xavier <mariana.xavier@creditas.com.br>
2023-10-23 18:08:30 -0300

    Updates routes on app

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,26 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home/home';
 import './App.css';
-import AboutMariana from './pages/about-mariana';
 import Header from './components/header';
+import { Members, AdailsonPage, JoaoVictorPage, MarceloPage, MarianaPage, RenanPage } from './pages/members';
+import { Container } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="home" element={<Home />} />
-        <Route path="about/mariana" element={<AboutMariana />} />
-      </Routes>
+      <Container fixed>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="home" element={<Home />} />
+          <Route path="integrantes" element={<Members />} />
+          <Route path="integrantes/adailson" element={<AdailsonPage />} />
+          <Route path="integrantes/joao-victor" element={<JoaoVictorPage />} />
+          <Route path="integrantes/marcelo" element={<MarceloPage />} />
+          <Route path="integrantes/mariana" element={<MarianaPage />} />
+          <Route path="integrantes/renan" element={<RenanPage />} />
+        </Routes>
+      </Container>
     </div>
   );
 }
