5d25be5dd0d43c29946c788771266c6049aa1c95
Mariana Xavier <mariana.xavier@creditas.com.br>
2023-10-23 15:23:43 -0300

    Adds members images

---
+++ src/assets/avatars/index.ts
@@ -0,0 +1,5 @@
+export { default as AdailsonAvatar } from './adailson_avatar.png';
+export { default as JoaoVictorAvatar } from './joao_victor_avatar.png';
+export { default as MarceloAvatar } from './marcelo_avatar.jpg';
+export { default as MarianaAvatar } from './mari_avatar.png';
+export { default as RenanAvatar } from './renan_avatar.png';
---
+++ src/assets/photos/index.ts
@@ -0,0 +1,5 @@
+export { default as AdailsonPhoto } from './adailson_foto.jpeg'
+export { default as JoaoVictorPhoto } from './joao_victor_foto.jpeg';
+export { default as MarceloPhoto } from './marcelo_foto.jpeg';
+export { default as MarianaPhoto } from './mari_foto.jpeg';
+export { default as RenanPhoto } from './renan_foto.jpeg';
