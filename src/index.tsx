defac8ad266401b7d6ceb220cadabe445051c769
Mariana Xavier <mariana.xavier@creditas.com.br>
2023-10-23 18:17:15 -0300

    Use HashRouter instead of BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/projeto-csm-2023'>
+    <HashRouter basename='/projeto-csm-2023'>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
