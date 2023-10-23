bd04859ecc61538a767a57c9dd04d062ec2bd9d3
Mariana Xavier <mariana.xavier@creditas.com.br>
2023-10-23 15:24:15 -0300

    Adds members data

---
+++ src/data/index.ts
@@ -0,0 +1,1 @@
+export { default as membersInfo } from './members';
---
+++ src/data/members.ts
@@ -0,0 +1,53 @@
+import { AdailsonPhoto, JoaoVictorPhoto, MarceloPhoto, MarianaPhoto, RenanPhoto } from '../assets/photos'
+import { AdailsonAvatar, JoaoVictorAvatar, MarceloAvatar, MarianaAvatar, RenanAvatar } from '../assets/avatars'
+
+
+const membersInfo = [
+  {
+    name: 'Adailson',
+    course: 'Engenharia de Informação',
+    favoriteColor: '',
+    favoriteAnimal: '',
+    curiosity: '',
+    photo: AdailsonPhoto,
+    avatar: AdailsonAvatar,
+  },
+  {
+    name: 'João Victor',
+    course: 'Engenharia de Informação',
+    favoriteColor: '',
+    favoriteAnimal: '',
+    curiosity: '',
+    photo: JoaoVictorPhoto,
+    avatar: JoaoVictorAvatar,
+  },
+  {
+    name: 'Marcelo',
+    course: 'Engenharia de Informação',
+    favoriteColor: '',
+    favoriteAnimal: '',
+    curiosity: '',
+    photo: MarceloPhoto,
+    avatar: MarceloAvatar,
+  },
+  {
+    name: 'Mariana',
+    course: 'Engenharia de Informação',
+    favoriteColor: 'Amarelo',
+    favoriteAnimal: 'Cachorro',
+    curiosity: 'Mergulhadora certificada e crescente interesse por RPG.',
+    photo: MarianaPhoto,
+    avatar: MarianaAvatar,
+  },
+  {
+    name: 'Renan',
+    course: 'Engenharia de Informação',
+    favoriteColor: '',
+    favoriteAnimal: '',
+    curiosity: '',
+    photo: RenanPhoto,
+    avatar: RenanAvatar,
+  },
+];
+
+export default membersInfo;
