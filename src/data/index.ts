7eb879d16c9da94dc2e39be4a40a85e034527929
Mariana Xavier <mariana.xavier@creditas.com.br>
2023-11-06 10:55:08 -0300

    Adds report infos

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,1 +1,2 @@
 export { default as membersInfo } from './members';
+export { default as reportsInfo } from './reports';
---
+++ src/data/reports.ts
@@ -0,0 +1,12 @@
+import { GroupCamera } from "../assets/reports/report1/fotos_camera";
+
+const reportsInfo = [
+  {
+    id: 1,
+    title: 'Relatório 1',
+    description: 'Neste laboratório, realizamos uma série de fotografias e gravações com diferentes dispositivos, visando poder comparar os resultados neste relatório.',
+    image: GroupCamera,
+  },
+];
+
+export default reportsInfo;
