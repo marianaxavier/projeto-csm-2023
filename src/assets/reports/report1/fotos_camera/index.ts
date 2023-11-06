2de1c325b604378e3d46ec5653373c31b0a1acc4
Mariana Xavier <mariana.xavier@creditas.com.br>
2023-11-06 09:27:37 -0300

    Rename report 1 assets

--- src/assets/reports/report1/fotos_camera/index.ts
+++ src/assets/reports/report1/fotos_camera/index.ts
@@ -1,21 +1,21 @@
-export { default as Adailson25 } from './Adailson_25.jpg';
-export { default as Adailson50 } from './Adailson_50.jpg';
-export { default as AdailsonPB} from './Adailson_pb.jpg';
-export { default as Adailson } from './Adailson.JPG';
-export { default as Group25 } from './group_25.jpg';
-export { default as Group50 } from './group_50.jpg';
-export { default as GroupPB } from './group_pb.jpg';
-export { default as Group } from './group.JPG';
-export { default as JoaoVictor25 } from './joao_victor_25.jpg';
-export { default as JoaoVictor50 } from './joao_victor_50.jpg';
-export { default as JoaoVictorPB} from './joao_victor_pb.jpg';
-export { default as JoaoVictor } from './joao_victor.jpeg';
+export { default as AdailsonCamera25 } from './Adailson_25.jpg';
+export { default as AdailsonCamera50 } from './Adailson_50.jpg';
+export { default as AdailsonCameraPB} from './Adailson_pb.jpg';
+export { default as AdailsonCamera } from './Adailson.JPG';
+export { default as GroupCamera25 } from './grupo_25.jpg';
+export { default as GroupCamera50 } from './grupo_50.jpg';
+export { default as GroupCameraPB } from './grupo_pb.jpg';
+export { default as GroupCamera } from './grupo.jpeg';
+export { default as JoaoVictorCamera25 } from './joao_victor_foto_25.jpg';
+export { default as JoaoVictorCamera50 } from './joao_victor_foto_50.jpg';
+export { default as JoaoVictorCameraPB} from './joao_victor_foto_pb.jpg';
+export { default as JoaoVictorCamera } from './joao_victor_foto.jpeg';
 export { default as LentoCamera540 } from './lento_camera_540.mp4'
 export { default as LentoCamera720 } from './lento_camera_720.mp4'
 export { default as LentoCamera } from './lento.mp4'
-export { default as Marcelo } from './Marcelo.JPG';
-export { default as Mari } from './Mari.JPG';
+export { default as MarceloCamera } from './Marcelo.JPG';
+export { default as MariCamera } from './Mari.JPG';
 export { default as RapidoCamera540 } from './rapido_camera_540.mp4'
 export { default as RapidoCamera720 } from './rapido_camera_720.mp4'
 export { default as RapidoCamera } from './rapido.mp4'
-export { default as Renan } from './Renan.JPG';
+export { default as RenanCamera } from './Renan.JPG';
