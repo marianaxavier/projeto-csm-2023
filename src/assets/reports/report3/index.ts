b28535725ff832b4aef50a193ac2975a4124579b
Mariana Xavier <mariana.xavier@creditas.com.br>
2023-11-13 07:52:44 -0300

    Renames report 2 to report 3

---
+++ src/assets/reports/report3/index.ts
@@ -0,0 +1,3 @@
+export { default as Report3 } from './report3.jpg';
+export { default as RGBConversion } from './rgb_conversion.png';
+export { default as RGBSeparation } from './rgb_separation.png';
