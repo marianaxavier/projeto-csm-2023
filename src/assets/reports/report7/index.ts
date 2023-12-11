58756249a255f824d061c249cac652e08a49f09f
Mariana Xavier <mariana.xavier@creditas.com.br>
2023-12-11 09:31:47 -0300

    Updates report 7 assets

--- src/assets/reports/report7/index.ts
+++ src/assets/reports/report7/index.ts
@@ -5,7 +5,7 @@ export { default as Img1 } from './Img1.png';
 export { default as Img2 } from './Img2.png';
 export { default as Img3 } from './Img3.png';
 export { default as Img4 } from './Img4.png';
-export { default as Img5 } from './Img5.png';
+export { default as Img5 } from './Img5.jpg';
 export { default as Img6 } from './Img6.png';
 export { default as Img7 } from './Img7.png';
 export { default as Img8 } from './Img8.png';
