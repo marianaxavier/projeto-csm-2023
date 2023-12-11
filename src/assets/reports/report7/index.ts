a2e72e7afbb81a1657891b94914adef6f3193c72
Mariana Xavier <mariana.xavier@creditas.com.br>
2023-12-11 09:15:16 -0300

    Adds report 5 assets

--- src/assets/reports/report7/index.ts
+++ src/assets/reports/report7/index.ts
@@ -1,1 +1,14 @@
 export { default as Report7 } from './Report7.jpg';
+export { default as AudioJoao } from './joao.wav';
+export { default as AudioMariana } from './mariana.wav';
+export { default as Img1 } from './Img1.png';
+export { default as Img2 } from './Img2.png';
+export { default as Img3 } from './Img3.png';
+export { default as Img4 } from './Img4.png';
+export { default as Img5 } from './Img5.png';
+export { default as Img6 } from './Img6.png';
+export { default as Img7 } from './Img7.png';
+export { default as Img8 } from './Img8.png';
+export { default as Img9 } from './Img9.png';
+export { default as Img10 } from './Img10.png';
+export { default as Img11 } from './Img11.png';
